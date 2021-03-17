1b8cff300dc76b6ae7450c2aa3eae96f89eccb1c
Tanya Yanko <61629908+Tana1509@users.noreply.github.com>
2021-03-17 15:04:43 +0200

    add: to top btn

--- index.js
+++ index.js
@@ -25,3 +25,21 @@ jQuery(document).ready(function ($) {
     owl.trigger("prev.owl.carousel");
   });
 });
+
+  // кнопка "наверх"
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 0) {
+      $(".to_top").fadeIn();
+    } else {
+      $(".to_top").fadeOut();
+    }
+  });
+  $(".to_top").click(function() {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      400
+    );
+    return false;
+  });
